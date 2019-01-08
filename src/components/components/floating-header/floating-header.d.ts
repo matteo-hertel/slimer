1f924ede63f78ccd2e6aa51dd7af69b23bf58a24
Matteo Hertel <info@matteohertel.com>
2019-01-08 11:38:56 +0100

    comp: add test to floating header

---
+++ src/components/components/floating-header/floating-header.d.ts
@@ -0,0 +1,14 @@
+import '../../../../dist/types/stencil.core';
+export declare class FloatingHeader {
+  /**
+   * The trigger treshold to activate the floating header
+   */
+  triggerTreshold: number;
+
+  /**
+   * The trigger treshold to activate the floating header
+   */
+  activeClassName: string;
+
+  render(): JSX.Element;
+}
