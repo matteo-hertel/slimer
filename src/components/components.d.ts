1ba6ec174dbf48df90d9b72d67461c09f0fd08e0
Matteo Hertel <info@matteohertel.com>
2019-01-19 16:41:51 +0000

    layout: use defaults rather than git info

--- src/components/components.d.ts
+++ src/components/components.d.ts
@@ -5,7 +5,7 @@
  */
 
 
-import '@stencil/core';
+import './stencil.core';
 
 
 
@@ -91,13 +91,5 @@ declare global {
     'git-edit': HTMLGitEditElement;
   }
 
-
-  export namespace JSX {
-    export interface Element {}
-    export interface IntrinsicElements extends StencilIntrinsicElements {
-      [tagName: string]: any;
-    }
-  }
-  export interface HTMLAttributes extends StencilHTMLAttributes {}
 
 }
