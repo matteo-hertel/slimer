fa40490828cabe21abe6c2cd1aecaeb851171bcd
Matteo Hertel <info@matteohertel.com>
2019-03-31 18:19:37 +0100

    create headers at build time

--- Makefile
+++ Makefile
@@ -1,9 +1,7 @@
 #!make
 prod:
-	rm -rf site/content
-	git clone https://github.com/matteo-hertel/blog.git ./site/content 
-	npm run build
-
-headers:
+	@rm -rf site/content
+	@git clone https://github.com/matteo-hertel/blog.git ./site/content 
+	@npm run build
 	@npm run generate:headers
 
---
+++ scripts/generateHeaders.js
@@ -0,0 +1,65 @@
+const webpackOutput = require('./../site/data/webpack.json');
+const headerTypeMap = new Map([
+  [
+    'js',
+    {
+      rel: 'preload',
+      as: 'script',
+    },
+  ],
+  [
+    'css',
+    {
+      rel: 'preload',
+      as: 'style',
+    },
+  ],
+  [
+    'img',
+    {
+      rel: 'preload',
+      as: 'image',
+    },
+  ],
+]);
+
+const globlaHeaders = [
+  ['X-Frame-Options', 'DENY'],
+  ['X-XSS-Protection', '1; mode=block'],
+];
+
+main();
+function main() {
+  const headers = createHeaderString([
+    ...globlaHeaders,
+    makeLinkHeader('js', webpackOutput['main']['js']),
+    makeLinkHeader('css', webpackOutput['main']['css']),
+  ]);
+
+  console.log(
+    `
+/*
+${headers}
+`,
+  );
+}
+
+function createHeaderString(headers) {
+  return headers.reduce(concatHeader, '');
+
+  function padString(string, pad = '    ') {
+    return `${pad}${string}`;
+  }
+
+  function concatHeader(acc, header) {
+    const [key, value] = header;
+    return `${acc}${padString(`${key}:${value}`)}\n`;
+  }
+}
+function makeLinkHeader(type, file) {
+  if (!headerTypeMap.has(type)) {
+    throw new Error('Unrecognised type');
+  }
+  const headerInfo = headerTypeMap.get(type);
+  return ['Link', `</${file}>; rel=${headerInfo.rel}; as=${headerInfo.as}`];
+}
