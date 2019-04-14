991250eeca6d0062b0fcf1155fac1f11b2042e08
Matteo Hertel <info@matteohertel.com>
2019-04-14 16:35:44 +0100

    security: update security headers (#10)
    * security: update security headers
    
    * squash: update Strict-Transport-Security
    
    * squash: fix double header
    
    * quash: remove unsupported push Feature Policy

--- scripts/generateHeaders.js
+++ scripts/generateHeaders.js
@@ -25,11 +25,12 @@ const headerTypeMap = new Map([
 
 const globlaHeaders = [
   ['Content-Security-Policy', 'upgrade-insecure-requests'],
-  ['Strict-Transport-Security', 'max-age=1000'],
   ['X-Xss-Protection', '1; mode=block'],
   ['X-Frame-Options', 'DENY'],
   ['X-Content-Type-Options', 'nosniff'],
   ['Referrer-Policy', 'strict-origin-when-cross-origin'],
+  ['Strict-Transport-Security', 'max-age=31536000; includeSubDomains; preload'],
+  ['Feature-Policy', "sync-xhr 'self'; fullscreen 'self'"],
 ];
 
 main();
